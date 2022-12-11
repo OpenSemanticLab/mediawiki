4fcdd0a1a5b972c2c8ddcb05d3c76f458ee0c194
SimonTaurus <simon.stier@gmx.de>
2022-12-11 14:52:02 +0100

    make action 'raw' slot-aware
    see https://phabricator.wikimedia.org/T324910

--- includes/actions/RawAction.php
+++ includes/actions/RawAction.php
@@ -193,7 +193,13 @@ class RawAction extends FormlessAction {
 			$request->response()->header( "Last-modified: $lastmod" );
 
 			// Public-only due to cache headers
-			$content = $rev->getContent( SlotRecord::MAIN );
+			// Fetch specific slot if defined
+			$slot = $this->getRequest()->getText( 'slot' );
+			if ( $slot ) {
+				if ( $rev->hasSlot( $slot ) ) $content = $rev->getContent( $slot );
+				else $content = null;
+			}
+			else $content = $rev->getContent( SlotRecord::MAIN );
 
 			if ( $content === null ) {
 				// revision not found (or suppressed)
